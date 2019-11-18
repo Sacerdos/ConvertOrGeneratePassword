11b98ea78ae19248096b31223f57c9826bec3125
Sacerdos <ilya.dychkov@gmail.com>
2019-11-18 17:08:06 +0300

    Added generating

--- app/src/main/java/com/indychkov/convertorgeneratepassword/MainActivity.java
+++ app/src/main/java/com/indychkov/convertorgeneratepassword/MainActivity.java
@@ -9,8 +9,10 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +24,12 @@ public class MainActivity extends Activity {
     private PasswordHelper helper;
     private TextView qualityText;
 
+    private TextView generatedResultTextView;
+    private View generateButton;
+    private SeekBar seekbarLengthGenerate;
+    private TextView sizeDescription;
+    private CompoundButton isCapitalize, isNumber, isSymbol;
+    private final int MIN_LENGTH = 8;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +73,52 @@ public class MainActivity extends Activity {
                         .show();
             }
         });
+        generatedResultTextView = findViewById(R.id.result_generated_textview);
+        generateButton = findViewById(R.id.button_copy_generated);
+        seekbarLengthGenerate = findViewById(R.id.length_generate);
+        sizeDescription = findViewById(R.id.password_size_descr);
+        isCapitalize = findViewById(R.id.is_capitalize);
+        isNumber = findViewById(R.id.is_number);
+        isSymbol = findViewById(R.id.is_symbol);
+        generateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String resultGenerate = helper.generatePassword(
+                        MIN_LENGTH + seekbarLengthGenerate.getProgress(),
+                        isCapitalize.isChecked(),
+                        isNumber.isChecked(),
+                        isSymbol.isChecked());
+                generatedResultTextView.setText(resultGenerate);
+                ClipboardManager manager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                manager.setPrimaryClip(ClipData.newPlainText(
+                        MainActivity.this.getString(R.string.clipboard_title_generated),
+                        generatedResultTextView.getText()
+                ));
+                Toast.makeText(MainActivity.this, R.string.message_copied, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+        seekbarLengthGenerate.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                sizeDescription.setText(
+                        getString(R.string.length_format,
+                                progress,
+                                getResources().getQuantityString(R.plurals.symbols_count, progress),
+                                MIN_LENGTH+progress,
+                                getResources().getQuantityString(R.plurals.symbols_count, MIN_LENGTH+progress)
+                                ));
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/indychkov/convertorgeneratepassword/PasswordHelper.java
+++ app/src/main/java/com/indychkov/convertorgeneratepassword/PasswordHelper.java
@@ -1,8 +1,11 @@
 package com.indychkov.convertorgeneratepassword;
 
+import java.util.Random;
+
 public class PasswordHelper {
     private final String[] RUSSIAN;
     private final String[] ENGLISH;
+    private Random random;
 
     public PasswordHelper(String[] russian, String[] english) {
         if (russian.length != english.length) {
@@ -10,7 +13,9 @@ public class PasswordHelper {
         }
         RUSSIAN = russian;
         ENGLISH = english;
+        random = new Random();
     }
+
     public String convert(CharSequence source) {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < source.length(); i++) {
@@ -19,7 +24,7 @@ public class PasswordHelper {
             boolean found = false;
             for (int j = 0; j < RUSSIAN.length; j++) {
                 if (RUSSIAN[j].equals(s)) {
-                    result.append(Character.isUpperCase(c)?
+                    result.append(Character.isUpperCase(c) ?
                             ENGLISH[j].toUpperCase() : ENGLISH[j]);
                     found = true;
                     break;
@@ -31,7 +36,30 @@ public class PasswordHelper {
         }
         return result.toString();
     }
-    public int getQuality(CharSequence password){
+
+    public int getQuality(CharSequence password) {
         return Math.min(password.length(), 10);
     }
+
+    public static final char[] SYMBOLS = {'@', '#', '_', '%', '&'};
+
+    public String generatePassword(int passLength, boolean isCaps, boolean isNumber, boolean isSymbol) {
+        StringBuilder password = new StringBuilder();
+        for (int i = 0; i < passLength; i++) {
+            boolean notLetter = (isNumber || isSymbol) && random.nextBoolean();
+            boolean toCaps = isCaps && random.nextBoolean();
+            if (notLetter) {
+                if ((random.nextInt(4) == 2 && isSymbol) || !isNumber) {
+                    password.append(SYMBOLS[random.nextInt(1000) % SYMBOLS.length]);
+                } else {
+                    password.append(Character.forDigit(random.nextInt(10), 10));
+                }
+            } else {
+                char c = Character.toChars('a' + random.nextInt(26))[0];
+                password.append(toCaps ? Character.toUpperCase(c) : c);
+            }
+        }
+        return password.toString();
+    }
+
 }
